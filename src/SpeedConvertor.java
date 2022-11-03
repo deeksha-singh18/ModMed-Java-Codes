17cd12cccd03337bd5f58a0312246e360ac22658
Deeksha Singh <deeksha.singh@office365.3pillarglobal.com>
2022-11-04 01:55:22 +0530

    first commit

---
+++ src/SpeedConvertor.java
@@ -0,0 +1,21 @@
+public class SpeedConvertor {
+
+    public static long milesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        } else {
+            return Math.round(kilometersPerHour / 1.609);
+
+        }
+    }
+
+    public static void conversion(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            System.out.println("It is an invalid value");
+        } else {
+            long res = milesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h = " + res + " mi/h");
+        }
+
+    }
+}
