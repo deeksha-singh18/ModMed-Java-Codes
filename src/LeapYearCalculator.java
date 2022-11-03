f923fbfee6db62a7193ed7edd1208b697808b5aa
Deeksha Singh <deeksha.singh@office365.3pillarglobal.com>
2022-11-04 01:19:56 +0530

    first commit

--- src/LeapYearCalculator.java
+++ src/LeapYearCalculator.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class LeapYearCalculator {
+public class LeapYearCalculator {
+    public static boolean isLeapYear(int year)
+    {
+        if(year>=1 && year<=9999)
+        {
+            if(((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
