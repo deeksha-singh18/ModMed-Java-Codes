264be797680949918be83ed91840fc7441ecd657
Deeksha Singh <deeksha.singh@office365.3pillarglobal.com>
2022-11-04 14:20:36 +0530

    first commit

---
+++ src/DataTypes/DataType.java
@@ -0,0 +1,35 @@
+package DataTypes;
+
+public class DataType {
+
+        public static void main(String[] args) {
+
+            Byte b1 = 34;
+            System.out.println(b1);
+
+            int i1 = Integer.MIN_VALUE;
+            int i2 = Integer.MAX_VALUE;
+
+            System.out.println(i1);
+            System.out.println(i2);
+
+            byte min_value = Byte.MIN_VALUE;
+            byte max_value = Byte.MAX_VALUE;
+
+            System.out.println(min_value);
+            System.out.println(max_value);
+
+            short smin_value = Short.MIN_VALUE;
+            short smax_value = Short.MAX_VALUE;
+
+            System.out.println(smin_value);
+            System.out.println(smax_value);
+
+
+            Long l1 = 100l;
+            System.out.println(l1);
+
+
+        }
+
+}
